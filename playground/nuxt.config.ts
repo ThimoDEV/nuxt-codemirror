4917b9b0dee4f7b65c7adf20f97b1d4c451d2dcb
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2024-12-28 14:34:19 +0100

    chore: updated playground packages

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
-  nuxtCodemirror: {},
   devtools: { enabled: true },
   compatibilityDate: '2024-07-24',
+  nuxtCodemirror: {},
 })
