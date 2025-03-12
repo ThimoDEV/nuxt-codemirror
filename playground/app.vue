72867030e9cfd4611f24bbf64c6c1a2562680504
Thimo <68739517+ThimoDEV@users.noreply.github.com>
2025-03-12 01:04:39 +0100

    chore: update packages and playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
 import { javascript } from '@codemirror/lang-javascript'
-import { lineNumbersRelative } from '@uiw/codemirror-extensions-line-numbers-relative'
-import { okaidia } from '@uiw/codemirror-theme-okaidia'
-
 import type { ViewUpdate } from '@codemirror/view'
+import { lineNumbersRelative } from '@uiw/codemirror-extensions-line-numbers-relative'
 import type { CodeMirrorRef, Statistics } from '../src/runtime/types/nuxt-codemirror'
 
 const code = ref('console.log("Hello, CodeMirror!");')
 // const theme = ref<'light' | 'dark' | 'none'>('light')
 const codemirror = ref<CodeMirrorRef>()
-
-const extensions = [lineNumbersRelative, javascript({ jsx: true, typescript: true })]
 
 const handleChange = (value: string, viewUpdate: ViewUpdate) => {
   console.log('Value changed:', value)
@@ -41,24 +37,23 @@ onMounted(() => {
 </script>
 
 <template>
-  <NuxtCodeMirror
-    ref="codemirror"
-    v-model="code"
-    :extensions="extensions"
-    style="width: 500px; height: 400px;"
-    :theme="okaidia"
-    placeholder="Enter your code here..."
-    :auto-focus="true"
-    :editable="true"
-    :basic-setup="true"
-    :indent-with-tab="true"
-    @on-change="handleChange"
-    @statistics="handleStatistics"
-    @on-update="handleUpdate"
-  />
-  <div>{{ code }}</div>
-  <input
-    v-model="code"
-    type="text"
-  >
+    <NuxtCodeMirror
+      ref="codemirror"
+      v-model="code"
+      :extensions="[lineNumbersRelative, javascript()]"
+      style="width: 500px; height: 400px;"
+      placeholder="Enter your code here..."
+      :auto-focus="true"
+      :basic-setup="true"
+      :editable="true"
+      :indent-with-tab="true"
+      @on-change="handleChange"
+      @statistics="handleStatistics"
+      @on-update="handleUpdate"
+    />
+    <div>{{ code }}</div>
+    <input
+      v-model="code"
+      type="text"
+    >
 </template>
--- src/runtime/getDefaultExtensions.ts
+++ src/runtime/getDefaultExtensions.ts
@@ -1,13 +1,68 @@
 import type { Extension } from '@codemirror/state'
-import { indentWithTab } from '@codemirror/commands'
-import { basicSetup, type BasicSetupOptions } from '@uiw/codemirror-extensions-basic-setup'
-import { EditorView, keymap, placeholder } from '@codemirror/view'
+import { indentWithTab, history, defaultKeymap, historyKeymap } from '@codemirror/commands'
+import type {
+  KeyBinding,
+} from '@codemirror/view'
+import {
+  EditorView,
+  keymap,
+  placeholder,
+  lineNumbers,
+  highlightActiveLineGutter,
+  highlightSpecialChars,
+  drawSelection,
+  dropCursor,
+  rectangularSelection,
+  crosshairCursor,
+  highlightActiveLine,
+} from '@codemirror/view'
 import { oneDark } from '@codemirror/theme-one-dark'
 import { EditorState } from '@codemirror/state'
+import { highlightSelectionMatches, searchKeymap } from '@codemirror/search'
+import { closeBrackets, autocompletion, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete'
+import {
+  foldGutter,
+  indentOnInput,
+  syntaxHighlighting,
+  defaultHighlightStyle,
+  bracketMatching,
+  indentUnit,
+  foldKeymap,
+} from '@codemirror/language'
+import { lintKeymap } from '@codemirror/lint'
 import { defaultLightThemeOption } from './theme/light'
 
 export * from '@codemirror/theme-one-dark'
 export * from './theme/light'
+
+export interface BasicSetupOptions {
+  lineNumbers?: boolean
+  highlightActiveLineGutter?: boolean
+  highlightSpecialChars?: boolean
+  history?: boolean
+  foldGutter?: boolean
+  drawSelection?: boolean
+  dropCursor?: boolean
+  allowMultipleSelections?: boolean
+  indentOnInput?: boolean
+  syntaxHighlighting?: boolean
+  bracketMatching?: boolean
+  closeBrackets?: boolean
+  autocompletion?: boolean
+  rectangularSelection?: boolean
+  crosshairCursor?: boolean
+  highlightActiveLine?: boolean
+  highlightSelectionMatches?: boolean
+
+  defaultKeymap?: boolean
+  closeBracketsKeymap?: boolean
+  searchKeymap?: boolean
+  historyKeymap?: boolean
+  foldKeymap?: boolean
+  completionKeymap?: boolean
+  lintKeymap?: boolean
+  tabSize?: number
+}
 
 export interface DefaultExtensionsOptions {
   indentWithTab?: boolean
@@ -16,6 +71,69 @@ export interface DefaultExtensionsOptions {
   theme?: 'light' | 'dark' | 'none' | Extension
   readOnly?: boolean
   editable?: boolean
+}
+
+export const minimalSetup: Extension = (() => [
+  highlightSpecialChars(),
+  history(),
+  drawSelection(),
+  syntaxHighlighting(defaultHighlightStyle, {fallback: true}),
+  keymap.of([
+    ...defaultKeymap,
+    ...historyKeymap,
+  ])
+])()
+
+/**
+ * Basic setup that includes standard CodeMirror extensions
+ */
+export const basicSetup = (options: BasicSetupOptions = {}): Extension[] => {
+  let keymaps: KeyBinding[] = []
+  if (options.closeBracketsKeymap !== false) {
+    keymaps = keymaps.concat(closeBracketsKeymap)
+  }
+  if (options.defaultKeymap !== false) {
+    keymaps = keymaps.concat(defaultKeymap)
+  }
+  if (options.searchKeymap !== false) {
+    keymaps = keymaps.concat(searchKeymap)
+  }
+  if (options.historyKeymap !== false) {
+    keymaps = keymaps.concat(historyKeymap)
+  }
+  if (options.foldKeymap !== false) {
+    keymaps = keymaps.concat(foldKeymap)
+  }
+  if (options.completionKeymap !== false) {
+    keymaps = keymaps.concat(completionKeymap)
+  }
+  if (options.lintKeymap !== false) {
+    keymaps = keymaps.concat(lintKeymap)
+  }
+
+  const extensions: Extension[] = []
+  if (options.lineNumbers !== false) extensions.push(lineNumbers())
+  if (options.highlightActiveLineGutter !== false) extensions.push(highlightActiveLineGutter())
+  if (options.highlightSpecialChars !== false) extensions.push(highlightSpecialChars())
+  if (options.history !== false) extensions.push(history())
+  if (options.foldGutter !== false) extensions.push(foldGutter())
+  if (options.drawSelection !== false) extensions.push(drawSelection())
+  if (options.dropCursor !== false) extensions.push(dropCursor())
+  if (options.allowMultipleSelections !== false) extensions.push(EditorState.allowMultipleSelections.of(true))
+  if (options.indentOnInput !== false) extensions.push(indentOnInput())
+  if (options.syntaxHighlighting !== false)
+    extensions.push(syntaxHighlighting(defaultHighlightStyle, { fallback: true }))
+  if (options.bracketMatching !== false) extensions.push(bracketMatching())
+  if (options.closeBrackets !== false) extensions.push(closeBrackets())
+  if (options.autocompletion !== false) extensions.push(autocompletion())
+  if (options.rectangularSelection !== false) extensions.push(rectangularSelection())
+  if (options.crosshairCursor !== false) extensions.push(crosshairCursor())
+  if (options.highlightActiveLine !== false) extensions.push(highlightActiveLine())
+  if (options.highlightSelectionMatches !== false) extensions.push(highlightSelectionMatches())
+  if (options.tabSize && typeof options.tabSize === 'number')
+    extensions.push(indentUnit.of(' '.repeat(options.tabSize)))
+
+  return extensions.concat([keymap.of(keymaps.flat())]).filter(Boolean)
 }
 
 export const getDefaultExtensions = (optios: DefaultExtensionsOptions = {}): Extension[] => {
@@ -33,10 +151,10 @@ export const getDefaultExtensions = (optios: DefaultExtensionsOptions = {}): Ext
   }
   if (defaultBasicSetup) {
     if (typeof defaultBasicSetup === 'boolean') {
-      getExtensions.unshift(basicSetup())
+      getExtensions.unshift(...basicSetup())
     }
     else {
-      getExtensions.unshift(basicSetup(defaultBasicSetup))
+      getExtensions.unshift(...basicSetup(defaultBasicSetup))
     }
   }
   if (placeholderStr) {
